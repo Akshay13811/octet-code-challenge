8f33d2e6519963e099eccc62b37eba046ece2d39
Akshay Saini <akshay@live.com.au>
2021-07-01 22:37:20 +1000

    Fixed error on markup setting page when server is down

--- app/src/components/MarkupSetting/index.tsx
+++ app/src/components/MarkupSetting/index.tsx
@@ -29,7 +29,7 @@ const MarkupSetting: React.FC<IMarkupSettingProps> = (props) => {
                 });
             }
         }, (response) => {
-            message.error(response);
+            message.error("An unexpected error ocurred", 10);
         });
     }
 
